7e90d83bbfa31ff14535d32da2c11611c41ec8b9
Harish Kumar <harish.pra22@gmail.com>
2018-06-27 02:59:44 +0530

    Tag orderable scopes added

--- src/Models/Tag.php
+++ src/Models/Tag.php
@@ -3,8 +3,9 @@
 namespace Hkp22\LaravelTaggable\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Hkp22\LaravelTaggable\Traits\TagOrderableScopes;
 
 class Tag extends Model
 {
-    //
+    use TagOrderableScopes;
 }
